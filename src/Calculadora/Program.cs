04fb037a5ee9c7c4a5d568d609efec489c8af7f0
Angelito Casagrande <angelito@angelito.com.br>
2021-01-11 16:41:11 -0300

    Implementa calculadora

--- src/Calculadora/Program.cs
+++ src/Calculadora/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace Calculadora
 {
@@ -6,7 +7,51 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bem vindo a Calculadora!");
+
+            Console.WriteLine("========================");
+
+            Console.WriteLine("Digite o primeiro valor:");
+            var primeiroValor = Console.ReadLine() ?? "0";
+
+            Console.WriteLine("Digite o segundo valor:");
+            var segundoValor = Console.ReadLine() ?? "0";
+
+            Console.WriteLine("Qual operação?");
+            Console.WriteLine("1) Adição");
+            Console.WriteLine("2) Subtração");
+            Console.WriteLine("3) Muliplicação");
+            Console.WriteLine("4) Divisão");
+            var operacao = Console.ReadLine() ?? string.Empty;
+
+            var resultado = 0.0;
+            switch (operacao)
+            {
+                case "1":
+                    resultado = Convert.ToDouble(primeiroValor) + Convert.ToDouble(segundoValor);
+                    break;
+                case "2":
+                    resultado = Convert.ToDouble(primeiroValor) - Convert.ToDouble(segundoValor);
+                    break;
+                case "3":
+                    resultado = Convert.ToDouble(primeiroValor) * Convert.ToDouble(segundoValor);
+                    break;
+                case "4":
+                    resultado = Convert.ToDouble(primeiroValor) / Convert.ToDouble(segundoValor);
+                    break;
+            }
+
+            Console.WriteLine("------------------------");
+            Console.WriteLine("O resultado é: " + resultado); // Não usar
+            Console.WriteLine("O resultado é: {0}", resultado);
+            Console.WriteLine($"O resultado é: {resultado}");
+
+            StringBuilder sb = new StringBuilder()
+                           .Append("O resultado é: ")
+                           .Append(resultado);
+            Console.WriteLine(sb.ToString());
+
+            Console.WriteLine("========================");
         }
     }
 }
