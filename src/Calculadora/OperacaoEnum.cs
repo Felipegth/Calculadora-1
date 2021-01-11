1051240e495ca3da9992846f88163fb60b3c6487
Angelito Casagrande <angelito@angelito.com.br>
2021-01-11 17:04:20 -0300

    Separa ações

---
+++ src/Calculadora/OperacaoEnum.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Calculadora
+{
+    public enum OperacaoEnum
+    {
+        SOMA = 1,
+        SUBTRACAO = 2,
+        MULTIPLICACAO = 3,
+        DIVISAO = 4
+    }
+}
--- src/Calculadora/Program.cs
+++ src/Calculadora/Program.cs
@@ -5,43 +5,66 @@ namespace Calculadora
 {
     class Program
     {
+        static double primeiroValor;
+        static double segundoValor;
+        static int operacao;
+        static IOperacao tipoOperacao;
+
         static void Main(string[] args)
+        {
+            CriaTelaEMenuOperacoes();
+
+            ExecutaOperacao();
+
+            ExibeResultado();
+        }
+
+        static void CriaTelaEMenuOperacoes()
         {
             Console.WriteLine("Bem vindo a Calculadora!");
 
             Console.WriteLine("========================");
 
             Console.WriteLine("Digite o primeiro valor:");
-            var primeiroValor = Convert.ToDouble(Console.ReadLine() ?? "0");
+            primeiroValor = Convert.ToDouble(Console.ReadLine() ?? "0");
 
             Console.WriteLine("Digite o segundo valor:");
-            var segundoValor = Convert.ToDouble(Console.ReadLine() ?? "0");
+            segundoValor = Convert.ToDouble(Console.ReadLine() ?? "0");
 
             Console.WriteLine("Qual operação?");
             Console.WriteLine("1) Adição");
             Console.WriteLine("2) Subtração");
             Console.WriteLine("3) Muliplicação");
             Console.WriteLine("4) Divisão");
-            var operacao = Console.ReadLine() ?? string.Empty;
+            operacao = Convert.ToInt32(Console.ReadLine() ?? "0");
+        }
 
-            IOperacao instanciaOperacao = null;
-            switch (operacao)
+        static void ExecutaOperacao()
+        {
+            switch ((OperacaoEnum)operacao)
             {
-                case "1":
-                    instanciaOperacao = new Soma(primeiroValor, segundoValor);
+                case OperacaoEnum.SOMA:
+                    tipoOperacao = new Soma(primeiroValor, segundoValor);
                     break;
-                case "2":
-                    instanciaOperacao = new Divisao(primeiroValor, segundoValor);
+
+                case OperacaoEnum.SUBTRACAO:
+                    tipoOperacao = new Divisao(primeiroValor, segundoValor);
                     break;
-                case "3":
-                    instanciaOperacao = new Multiplicacao(primeiroValor, segundoValor);
+
+                case OperacaoEnum.MULTIPLICACAO:
+                    tipoOperacao = new Multiplicacao(primeiroValor, segundoValor);
                     break;
-                case "4":
-                    instanciaOperacao = new Divisao(primeiroValor, segundoValor);
+
+                case OperacaoEnum.DIVISAO:
+                    tipoOperacao = new Divisao(primeiroValor, segundoValor);
                     break;
             }
+        }
 
-            var operacoes = new Operacoes(instanciaOperacao);
+        static void ExibeResultado()
+        {
+            var operacoes = new Operacoes(tipoOperacao);
+
             Console.WriteLine("------------------------");
             Console.WriteLine("O resultado é: " + operacoes.Resultado()); // Não usar
             Console.WriteLine("O resultado é: {0}", operacoes.Resultado());
